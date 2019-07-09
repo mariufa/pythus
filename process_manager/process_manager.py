1cc5ae12f76d02242b9ee83817656eab802c245c
Marius Fagerland <mariusfagerland@yahoo.no>
2019-07-09 15:54:11 +0200

    Add dynamic imports

--- process_manager/process_manager.py
+++ process_manager/process_manager.py
@@ -1,15 +1,21 @@
 import pika
-import processors
+import pkgutil
+import importlib
+import threading
 
 class ProcessManager:
 
     def __init__(self):
+        self.setup_processors()
+        self.watch_input_directory()
         self.rabbitMQConnection()
 
     def setup_processors(self):
         self.processors = []
-        for p in processors.iter:
-            print(p)
+        processors_module_name = 'processors'
+        list_processors_name = 'test'
+        for p in pkgutil.walk_packages([processors_module_name]):
+            self.processors.append(importlib.import_module(processors_module_name + '.' + p.name))
 
     def rabbitMQConnection(self):
         connetion = pika.BlockingConnection(
@@ -22,3 +28,5 @@ class ProcessManager:
     def handle_event(self, ch, method, properties, body):
         print(" [x]  Received %r" % body)
         
+    def watch_input_directory(self):
+        pass
--- processors/hello_processor.py
+++ processors/hello_processor.py
@@ -1,4 +1,4 @@
 
 
-def hello():
+def run():
     print("hello");
