d6088c3f9c3b1e26c66d5e072f335a77c9a4c0c6
Marius Fagerland <mariusfagerland@yahoo.no>
2019-07-12 08:41:19 +0200

    Add nifi files

---
+++ nifi/flow_file_read.py
@@ -0,0 +1,44 @@
+import os
+import struct
+
+# Example usage
+# import hashlib
+#
+# with open('flowfiles.pkg', 'rb') as f:
+#     for attrs, offset, size in read_flow_file_stream(f):
+#         m = hashlib.md5()
+#
+#         f.seek(offset)
+#         m.update(f.read(size))
+#
+#         print("{}=={}".format(m.hexdigest(), attrs['hash.value']))
+
+MAGIC_HEADER = b'NiFiFF3'
+
+def read_field_length(fp):
+    i = fp.read(2)
+    if i == bytearray.fromhex('ffff'):
+        return struct.unpack('>I', fp.read(4))[0]
+    else:
+        return struct.unpack('>H', i)[0]
+
+def read_string(fp):
+    length = read_field_length(fp)
+    return fp.read(length).decode('UTF-8')
+
+def read_flow_file_stream(fp):
+    while True:
+        if fp.read(len(MAGIC_HEADER)) != MAGIC_HEADER:
+            break
+
+        attrs = {}
+        num_attrs = read_field_length(fp)
+        for _ in range(num_attrs):
+            key = read_string(fp)
+            value = read_string(fp)
+            attrs[key] = value
+
+        size = struct.unpack('>Q', fp.read(8))
+        offset = fp.tell()
+        yield attrs, offset, size
+        fp.seek(offset + size)
---
+++ nifi/flow_file_write.py
@@ -0,0 +1,35 @@
+import shutil 
+import struct
+
+# Example usage
+# import io 
+#
+# with open('flowfiles.pkg', 'wb') as f:
+#     data = b"Hello world!"
+#     write_flow_file_stream(f, {'filename': 'file1'}, len(data), io.BytesIO(data))
+#     write_flow_file_stream(f, {'filename': 'file2'}, len(data), io.BytesIO(data))
+
+MAGIC_HEADER = b'NifiFF3'
+MAX_VALUE_2_BYTES = 65535
+
+def write_string(fp, data):
+    data = data.encode('UTF-8')
+    length = len(data)
+
+    if length < MAX_VALUE_2_BYTES:
+        fp.write(struct.pack('>H', length))
+    else:
+        fp.write(b'\xff\xff')
+        fp.write(struct.pack('>I', length))
+    fp.write(data)
+
+def write_flow_file_stream(fp, attrs, size, fileobj):
+    fp.write(MAGIC_HEADER)
+    fp.write(struct.pack('>H', len(attrs)))
+
+    for key, value in attrs.items():
+        write_string(fp, key)
+        write_string(fp, value)
+
+    fp.write(struct.pack('>Q', size))
+    shutil.copyfileobj(fileobj, fp)
