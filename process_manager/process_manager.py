254a105eeec63926025385b1486b59f73255d5b2
Marius Fagerland <mariusfagerland@yahoo.no>
2019-07-16 22:21:33 +0200

    Create file_handler
    Move file handling from process_manager

--- process_manager/process_manager.py
+++ process_manager/process_manager.py
@@ -3,21 +3,19 @@ import pkgutil
 import importlib
 import threading
 import json
-from nifi.flow_file_read import *
-from nifi.flow_file_write import *
 from utils.rabbitmq import sendEvent
 import uuid
 
 import time
-from os import walk, rename, remove
-from os.path import join, getsize
-import io
+
+from file_handler import FileHandler
 
 class ProcessManager:
 
     def __init__(self):
         self.setup_processors()
-        self.watch_input_directory()
+        self.file_handler = FileHandler()
+        self.file_handler.watch_input_directory()
         self.rabbitMQConnection()
 
     def setup_processors(self):
@@ -60,76 +58,5 @@ class ProcessManager:
                 time.sleep(2)
                 sendEvent(message)
         else:
-            output_dir = "./data/output"
-            attrs = self.generate_attrs(message)
-            print(" [x]  Processed %r" % message)
-
-            with open(message["path"], 'rb') as done_file:
-                file_size  = getsize(message["path"])
-                with open(join(output_dir, str(uuid.uuid4())), 'wb') as output_file:
-                    write_flow_file_stream(output_file, attrs, file_size, done_file)
-            remove(message["path"])
-            
-
-    def generate_attrs(self, message):
-        attrs = {}
-        attrs["filename"] = message["filename"]
-        attrs["filetype"] = message["filetype"]
-        attrs["identifier"] = message["identifier"]
-        
-        if "parent" in message:
-            attrs["parent"] = message["parent"]
-        
-        if "metadata" in message:
-            attrs["metadata"] = message["metadata"]
-        
-        return attrs
-        
-    def watch_input_directory(self):
-        watch_thread = threading.Thread(target=watch_input_directory)
-        watch_thread.start()
-
-
-def watch_input_directory():
-    DIRECTORY_TO_WATCH = "./data/input"
-    DIRECTORY_TO_MOVE_TO = "./data/processing"
-    while True:
-        time.sleep(2)
-        for root, dirs, filenames in walk(DIRECTORY_TO_WATCH):
-            for name in filenames:
-                if (name[0] != '.'):
-                    with open(join(root, name), 'rb') as f:
-                        for attrs, offset, size in read_flow_file_stream(f):
-                            f.seek(offset)
-                            new_file_name = join(DIRECTORY_TO_MOVE_TO, str(uuid.uuid4()))
-                            with open(new_file_name, 'wb') as new_f:
-                                chunk_size = 4096
-                                if size < chunk_size:
-                                    chunk_size = size
-                                chunk = f.read(chunk_size)
-                                while chunk:
-                                    new_f.write(chunk)
-                                    size = size - chunk_size
-                                    if size == 0:
-                                        break
-                                    elif size < chunk_size:
-                                        chunk_size = size
-                                    chunk = f.read(chunk_size)
-
-                            identifier = str(uuid.uuid4())
-                            if "identifier" in attrs:
-                                identifier = attrs["identifier"]
-
-                            message = {
-                                "identifier": identifier,
-                                "path": new_file_name,
-                                "filename" : attrs["filename"],
-                                "filetype": "unknown",
-                                "nifi_attrs": attrs,
-                                "history": [],
-                                "metadata": {}
-                            }
-
-                            sendEvent(message)
-                            remove(join(root, name))
+            self.file_handler.handle_output_file(message)
 
