578ee77bbe9fd8155e65b2e93e453395a6311eff
Marius Fagerland <mariusfagerland@yahoo.no>
2019-07-16 14:54:02 +0200

    Implement ocr

--- processors/ocr_processor.py
+++ processors/ocr_processor.py
@@ -1,4 +1,7 @@
 from utils.rabbitmq import sendEvent
+from PIL import Image
+import pytesseract
+import uuid
 
 def get_mime_types():
     return [
@@ -7,5 +10,22 @@ def get_mime_types():
     ]
 
 def run(message):
-    #TODO ocr
+    text = pytesseract.image_to_string(Image.open(message["path"]))
+    if text:
+        processing_dir = "./data/processing/"
+        identifier = str(uuid.uuid4())
+        with open(processing_dir + identifier, 'wb') as f:
+            f.write(text.encode('UTF-8'))
+
+        new_message = {
+            "identifier": identifier,
+            "parent": message["identifier"],
+            "path": processing_dir + identifier,
+            "filename" : "ocr.txt",
+            "filetype": "plain/text",
+            "history": [],
+            "metadata": {}
+        }
+        sendEvent(new_message)
+
     sendEvent(message)
