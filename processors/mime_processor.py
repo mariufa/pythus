da85c524dadc4697e52ae583ffc2accc5c6ad373
Marius Fagerland <mariusfagerland@yahoo.no>
2019-08-09 14:09:02 +0200

    Improve mime processor

--- processors/mime_processor.py
+++ processors/mime_processor.py
@@ -1,10 +1,23 @@
 from utils.rabbitmq import sendEvent
 import magic
+import mimetypes
 
 def want(message):
     return True
 
 def run(message):
-    message["filetype"] = magic.from_file(message["path"], mime=True)
+    result = magic.from_file(message["path"], mime=True)
+    if result == "application/octet-stream":
+        mime_result = mimetypes.MimeTypes().guess_type(message["path"])[0]
+        if mime_result != None:
+            result = mime_result
+
+    message["filetype"] = result
     sendEvent(message)
 
+
+if __name__ == "__main__":
+    result = magic.from_file('test-tull.iso', mime=True)
+    result2 = mimetypes.MimeTypes().guess_type('./aa/b/test-tull.iso')[0]
+    print(result)
+    print(result2)
