d6fd89cdc245b5ab92a1df0fbedbf4f3fddfe396
Marius Fagerland <mariusfagerland@yahoo.no>
2019-08-09 10:09:24 +0200

    Finish language detection

--- processors/language_processor.py
+++ processors/language_processor.py
@@ -3,11 +3,19 @@ from langdetect import detect
 
 def want(message):
     supported_filetype = [
-        "plain/text"
+        "text/plain"
     ]
     filetype = message["filetype"]
     return filetype in supported_filetype
 
 def run(message):
-    
+    MAX_TEXT_BUFFER = 50000000
+    with open(message["path"], 'rb') as f:
+        text_bytes = f.read(MAX_TEXT_BUFFER)
+
+    text = text_bytes.decode("utf-8")
+    if text != None and len(text) > 20:
+        language = detect(text)
+        metadata = message["metadata"]
+        metadata["language"] = language
     sendEvent(message)
