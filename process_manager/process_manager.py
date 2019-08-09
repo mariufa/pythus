36c5c7bd6123fec7479ada30dd8a061d30684776
Marius Fagerland <mariusfagerland@yahoo.no>
2019-08-09 10:09:39 +0200

    Increase log level for pika

--- process_manager/process_manager.py
+++ process_manager/process_manager.py
@@ -13,6 +13,7 @@ from process_manager.file_handler import FileHandler
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+logging.getLogger("pika").setLevel(logging.WARNING)
 
 class ProcessManager:
 
