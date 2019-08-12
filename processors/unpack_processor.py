5bac608c5349e6e8f41e042057a182fb04fe53df
Marius Fagerland <mariusfagerland@yahoo.no>
2019-08-12 17:09:04 +0200

    Implement unpack_processor

--- processors/unpack_processor.py
+++ processors/unpack_processor.py
@@ -1,12 +1,49 @@
-#from utils.rabbitmq import sendEvent
+from utils.rabbitmq import sendEvent
 from pyunpack import Archive
 import os
+import uuid
+import shutil
 
-# def want(message):
-#     return False
+def want(message):
+    supported_mime_types = [
+        "application/zip"
+    ]
+    filetype = message["filetype"]
+    return filetype in supported_mime_types
 
-# def run(message):
-#     sendEvent(message)
+def run(message):
+    processng_dir = "./data/processing"
+    tmp_identifier = os.path.join(processng_dir, str(uuid.uuid4()))
+    tmp_folder = os.path.join(tmp_identifier, "tmp")
+    os.makedirs(tmp_folder, exist_ok = True)
+
+    file_dest = os.path.join(tmp_identifier, message["filename"])
+    shutil.copyfile(message["path"], file_dest)
+
+    Archive(file_dest).extractall(tmp_folder)
+
+    for root, dirs, files in os.walk(tmp_folder):
+        for filename in files:
+
+            new_identifier = str(uuid.uuid4())
+            processing_dest = os.path.join(processng_dir, new_identifier)
+            metadata = {}
+            metadata["archive_file_path"] = root.replace(tmp_folder, '')
+            new_message = {
+                "identifier": new_identifier,
+                "parent": message["identifier"],
+                "path": processing_dest,
+                "filename" : filename,
+                "filetype": "unknown",
+                "history": [],
+                "metadata": metadata,
+                "original_file": False
+            }
+            shutil.move(os.path.join(root, filename), processing_dest)
+            sendEvent(new_message)
+
+    shutil.rmtree(tmp_identifier)
+    sendEvent(message)
 
 
 if __name__ == "__main__":
