00455b197facec40d8af143def3576ae9900057a
Marius Fagerland <mariusfagerland@yahoo.no>
2019-08-13 09:47:37 +0200

    Fix mimetype bug

--- processors/mime_processor.py
+++ processors/mime_processor.py
@@ -8,7 +8,7 @@ def want(message):
 def run(message):
     result = magic.from_file(message["path"], mime=True)
     if result == "application/octet-stream":
-        mime_result = mimetypes.MimeTypes().guess_type(message["path"])[0]
+        mime_result = mimetypes.MimeTypes().guess_type(message["filename"])[0]
         if mime_result != None:
             result = mime_result
 
