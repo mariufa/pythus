7940c8793cb353ffe43d954239e0f3be8cae0d3e
Marius Fagerland <mariusfagerland@yahoo.no>
2019-08-13 13:26:22 +0200

    Fix pdf processor

--- processors/pdf_processor.py
+++ processors/pdf_processor.py
@@ -4,6 +4,7 @@ import uuid
 import PyPDF2
 from PIL import Image
 import shutil
+import fitz
 
 def want(message):
     supported_mime_types = [
@@ -34,12 +35,43 @@ def run(message):
                 "original_file": False
             }
         sendEvent(new_message)
-    extract_images(message)
+    extract_images_pymupdf(message)
 
     
     sendEvent(message)
 
-def extract_images(message):
+def extract_images_pymupdf(message):
+    doc = fitz.open(message["path"])
+    for i in range(len(doc)):
+        for img in doc.getPageImageList(i):
+            processing_dir = "./data/processing/"
+            identifier = str(uuid.uuid4())
+            workfile = processing_dir + identifier
+
+            xref = img[0]
+            pix = fitz.Pixmap(doc, xref)
+            if pix.n < 5:
+                pix.writePNG(workfile)
+            else:
+                pix1 = fitz.Pixmap(fitz.czRGB, pix)
+                pix1.writePNG(workfile)
+                pix1 = None
+            pix = None
+            filename = str(i) + "-" + str(xref) + ".png"
+            new_message = {
+                "identifier": identifier,
+                "parent": message["identifier"],
+                "path": workfile,
+                "filename" : filename,
+                "filetype": "unknown",
+                "history": [],
+                "metadata": {},
+                "original_file": False
+            }
+            sendEvent(new_message)
+    doc.close()
+
+def extract_images_pypdf2(message):
     input1 = PyPDF2.PdfFileReader(open(message["path"], "rb"))
     for page_index in range(input1.getNumPages()):
         page = input1.getPage(page_index)
@@ -53,7 +85,7 @@ def extract_images(message):
                 workfile = processing_dir + identifier
 
                 size = (xObject[obj]['/Width'], xObject[obj]['/Height'])
-                data = xObject[obj].getData()
+                
                 if xObject[obj]['/ColorSpace'] == '/DeviceRGB':
                     mode = "RGB"
                 else:
@@ -62,17 +94,20 @@ def extract_images(message):
                 filename = None
                 tmp_workfile = workfile
                 if xObject[obj]['/Filter'] == '/FlateDecode':
+                    data = xObject[obj].getData()
                     img = Image.frombytes(mode, size, data)
                     tmp_workfile += ".png"
                     img.save(tmp_workfile)
                     filename = obj[1:] + ".png"
                 elif xObject[obj]['/Filter'] == '/DCTDecode':
+                    data = xObject[obj]._data
                     tmp_workfile += ".jpg"
                     img = open(tmp_workfile, "wb")
                     filename = obj[1:] + ".jpg"
                     img.write(data)
                     img.close()
                 elif xObject[obj]['/Filter'] == '/JPXDecode':
+                    data = xObject[obj].getData()
                     tmp_workfile += ".jp2"
                     img = open(tmp_workfile, "wb")
                     filename = obj[1:] + ".jp2"
